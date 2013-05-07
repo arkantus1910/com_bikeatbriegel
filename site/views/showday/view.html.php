e61f5e81008cb6a5cf2023b2fc04b301b870a311
Spippo <thaens.tim@gmail.com>
2013-05-07 15:52:18 +0300

    Update view.html.php

--- site/views/showday/view.html.php
+++ site/views/showday/view.html.php
@@ -8,7 +8,7 @@ jimport('joomla.application.component.view');
 /**
  * HTML View class for the HelloWorld Component
  */
-class BikeAtBriegelViewBikeAtBriegel extends JViewLegacy
+class BikeAtBriegelViewShowDay extends JViewLegacy
 {
         // Overwriting JView display method
         function display($tpl = null)
