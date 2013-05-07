e91d54e9048917f5813b871be6d983bc1cfec79e
TimThaens <thaens.tim@gmail.com>
2013-05-07 14:35:36 +0200

    Start of showday

---
+++ site/views/showday/view.html.php
@@ -0,0 +1,23 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla view library
+jimport('joomla.application.component.view');
+ 
+/**
+ * HTML View class for the HelloWorld Component
+ */
+class BikeAtBriegelViewBikeAtBriegel extends JViewLegacy
+{
+        // Overwriting JView display method
+        function display($tpl = null)
+        {
+                // Assign data to the view
+                $this->Title = 'Activiteiten op 2013-05-07';
+                $this->Body = 'Test';
+
+                parent::display($tpl);
+        }
+}
+?>
